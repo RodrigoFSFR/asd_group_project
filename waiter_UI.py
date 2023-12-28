c0b52611aa9f98ae678dd4963c448c8242f8a40b
guilherme.enes <guilherme.enes@caixamagica.pt>
2023-12-28 17:20:20 +0000

    Added user logo image and profile ui for the staff members

--- waiter_UI.py
+++ waiter_UI.py
@@ -22,7 +22,38 @@ logo_resize = logo.subsample(2, 2)
 smallLogo = Label(window, image=logo_resize)
 smallLogo.place(x=90, y=50)
 
-ButtonProfile = tk.Button(window, width=40, text="Profile", font=("Sans-serif", 16))
+def profileWindow():
+
+    profileWindow = Toplevel(window)
+    profileWindow.title("Profile Page")
+ 
+    profileWindow.geometry("410x500+700+100")
+    # profileWindow.resizable(False, False)
+
+    profileWindow.columnconfigure(0, weight=1)
+    profileWindow.rowconfigure(0, weight=0)
+ 
+    userLogo = tk.PhotoImage(file="user.png")
+    label = Label(profileWindow, image=userLogo)
+    label.img = userLogo
+    label.grid(row=1, column=0)
+
+    jobText = Label(profileWindow, text="Job Title", font=("Sans-serif", 18))
+    jobText.grid(row=2, column=0, sticky="n")
+
+    usernameText = Label(profileWindow, text="Username", font=("Sans-serif", 18))
+    usernameText.grid(row=3, column=0, sticky="n")
+
+    idText = Label(profileWindow, text="ID Number: ID", font=("Sans-serif", 18))
+    idText.grid(row=4, column=0, sticky="n")
+
+    def Back ():
+        profileWindow.destroy()
+
+    ButtonExit = tk.Button(profileWindow, text="Back", width=10, command=Back, font=("Sans-serif", 16))
+    ButtonExit.grid(row=5, column=0, sticky="n")
+
+ButtonProfile = tk.Button(window, width=40, command=profileWindow, text="Profile", font=("Sans-serif", 16))
 ButtonTakeOrders = tk.Button(window, text="Take Orders", width=40, font=("Sans-serif", 16))
 ButtonPayment = tk.Button(
     window, text="Payment", width=40, font=("Sans-serif", 16)
