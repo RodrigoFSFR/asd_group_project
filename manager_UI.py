72afefda853adf7494e0bb3778abd18a5969198a
guilherme.enes <guilherme.enes@caixamagica.pt>
2024-01-06 21:09:26 +0000

    Linked the manager page to the manage staff page

--- manager_UI.py
+++ manager_UI.py
@@ -10,29 +10,28 @@ window.geometry("800x210+500+100")
 window.resizable(False, False)
 
 
-
 welcomeText = Label(window, text="Welcome, Username", font=("Sans-serif", 18))
-welcomeText.grid(padx= 40, sticky="w")
+welcomeText.grid(padx=40, sticky="w")
 
 logo = tk.PhotoImage(file="logoSVG.png")
 
-#resize the image to fit the screen better
+# resize the image to fit the screen better
 logo_resize = logo.subsample(2, 2)
 
 smallLogo = Label(window, image=logo_resize)
 smallLogo.place(x=90, y=50)
 
-def profileWindow():
 
+def profileWindow():
     profileWindow = Toplevel(window)
     profileWindow.title("Profile Page")
- 
+
     profileWindow.geometry("410x500+700+100")
     profileWindow.resizable(False, False)
 
     profileWindow.columnconfigure(0, weight=1)
     profileWindow.rowconfigure(0, weight=0)
- 
+
     userLogo = tk.PhotoImage(file="user.png")
     label = Label(profileWindow, image=userLogo)
     label.img = userLogo
@@ -47,33 +46,57 @@ def profileWindow():
     idText = Label(profileWindow, text="ID Number: ID", font=("Sans-serif", 18))
     idText.grid(row=4, column=0, sticky="n")
 
-    def Back ():
+    def Back():
         profileWindow.destroy()
 
-    ButtonExit = tk.Button(profileWindow, text="Back", width=10, command=Back, font=("Sans-serif", 16))
+    ButtonExit = tk.Button(
+        profileWindow, text="Back", width=10, command=Back, font=("Sans-serif", 16)
+    )
     ButtonExit.grid(row=5, column=0, sticky="n")
 
 
+ButtonProfile = tk.Button(
+    window, width=40, command=profileWindow, text="Profile", font=("Sans-serif", 16)
+)
 
-ButtonProfile = tk.Button(window, width=40, command=profileWindow, text="Profile", font=("Sans-serif", 16))
+
+def openManageStaff():
+    window.destroy()
+    import manageStaff
+
+
 ButtonManageStaff = tk.Button(
-    window, width=40, text="Manage Staff", font=("Sans-serif", 16)
+    window,
+    width=40,
+    text="Manage Staff",
+    command=openManageStaff,
+    font=("Sans-serif", 16),
 )
 ButtonOrders = tk.Button(window, text="Orders", width=40, font=("Sans-serif", 16))
+
 
 def openReservation():
     window.destroy()
     import reservation
 
+
 ButtonReservations = tk.Button(
-    window, text="Reservations", width=40, font=("Sans-serif", 16), command=openReservation
+    window,
+    text="Reservations",
+    width=40,
+    font=("Sans-serif", 16),
+    command=openReservation,
 )
 
+
 # Exit function for the exit button
-def Exit ():
+def Exit():
     window.quit()
 
-ButtonExit = tk.Button(window, text="Exit", width=40, command=Exit, font=("Sans-serif", 16))
+
+ButtonExit = tk.Button(
+    window, text="Exit", width=40, command=Exit, font=("Sans-serif", 16)
+)
 
 
 ButtonProfile.grid(row=0, column=1, sticky="E")
